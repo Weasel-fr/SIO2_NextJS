4cebcef9a40c274b23abb940cef942d70f91cf54
Dominique Rigaudière <d-rigaudiere@dijonformation.com>
2024-01-09 11:09:57 +0100

    toast

--- app/clients/NewClient.tsx
+++ app/clients/NewClient.tsx
@@ -2,12 +2,13 @@
 
 import { PrismaClient } from "@prisma/client";
 import type { Client } from "@prisma/client";
+import { useState } from "react";
 
 const prisma = new PrismaClient();
 
-
-
-export default function NewClient(props: {newClient:Function}) {
+export default function NewClient(props: { newClient: Function }) {
+  const [displayToast, setDisplayToast] = useState(false);
+  const [messageToast, setMessageToast] = useState("");
 
   async function addClient(formData: FormData) {
     // appel de l'api pour mettre le nouveau client en base
@@ -18,38 +19,62 @@ export default function NewClient(props: {newClient:Function}) {
         email: formData.get("email"),
         password: formData.get("password"),
       }),
-      });
+    });
 
-      const client:Client = await response.json();
+    if (response.status === 200) {
+      const client: Client = await response.json();
       props.newClient(client);
+      setMessageToast("Nouveau client ajouté");
+      setDisplayToast(true);
+      setTimeout(() => {
+        setDisplayToast(false);
+      }, 2000);
+    }
+    else {
+      setMessageToast("Erreur lors de l'ajout du client");
+      setDisplayToast(true);
+      setTimeout(() => {
+        setDisplayToast(false);
+      }, 2000);
+    }
+
   }
 
   return (
-    <form className="m-20" action={addClient}>
-      <label htmlFor="name">Nom du client : </label>
-      <input
-        name="name"
-        type="text"
-        placeholder="Jean Bonbeurre"
-        className="input input-bordered w-full max-w-xs"
-      />
-      <label htmlFor="email">Email du client : </label>
-      <input
-        name="email"
-        type="text"
-        placeholder="toto@domaine.com"
-        className="input input-bordered w-full max-w-xs"
-      />
-      <label htmlFor="password">Password du client : </label>
-      <input
-        name="password"
-        type="password"
-        placeholder="password"
-        className="input input-bordered w-full max-w-xs"
-      />
-      <button type="submit" className="btn btn-active">
-        Ajouter
-      </button>
-    </form>
+    <>
+      <form className="m-20" action={addClient}>
+        <label htmlFor="name">Nom du client : </label>
+        <input
+          name="name"
+          type="text"
+          placeholder="Jean Bonbeurre"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <label htmlFor="email">Email du client : </label>
+        <input
+          name="email"
+          type="text"
+          placeholder="toto@domaine.com"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <label htmlFor="password">Password du client : </label>
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <button type="submit" className="btn btn-active">
+          Ajouter
+        </button>
+      </form>
+      {displayToast && (
+        <div className="toast">
+          <div className="alert alert-info">
+            <span>{messageToast}</span>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
